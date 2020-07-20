14db619b6fb02638f0cae2433018b5d6f38bb5ff
Kiyo Kanno <kiyotakakanno2010@gmail.com>
2020-07-20 19:50:28 +1000

    Added TextBox Components

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+//custom component
+import { TextBox } from './components/TextBox';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Click me" color="blue"/>
       <StatusBar style="auto" />
+      <TextBox color="green" size="24" text="Custom Component" />
+      <TextBox color="red" size="12" text="Custom Text 2" />
     </View>
   );
 }
